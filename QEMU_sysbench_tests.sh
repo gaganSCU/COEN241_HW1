d129268c75e6fece15ae4c55616158e0e21ad9ad
Gagan Gupta <ggupta2@scu.edu>
2021-10-16 18:46:04 -0700

    QEMU Test for Config 1

--- QEMU_sysbench_tests.sh
+++ QEMU_sysbench_tests.sh
@@ -1,11 +1,12 @@
+outputFile=$1
 sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-touch test_log.txt
-> test_log.txt
+touch $outputFile
+> $outputFile
 runCPU(){
 	number=$1
 	shift
 	for i in {1..5}; do
-		sysbench --test=cpu --cpu-max-prime=$number run | tail -n 2 | head -n 1 >> test_log.txt
+		sysbench --test=cpu --cpu-max-prime=$number run | tail -n 2 | head -n 1 >> $outputFile
 	done
 }
 
@@ -14,24 +15,30 @@ runFileIO(){
 	mode=$2
 	shift
 	for i in {1..5}; do
-		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw prepare #| head -n 3 | tail -n 1 >> test_log.txt
-		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw run >> test_log.txt #| tee >(head -n 8 | tail -n 2) >(tail -n 12 | head -n 1) >(tail -n 2 | head -n 1) | tail -n 2 >> test_log.txt
+		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw prepare
+		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw run >> $outputFile
 		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw cleanup
 		sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 	done
 }
 
-echo "---CPU Tests---" >> test_log.txt
-echo "-----5000" >> test_log.txt
+echo "CPU Core Count" >> $outputFile
+sudo lshw -c cpu | grep configuration >> $outputFile
+echo "RAM Amount" >> $outputFile
+sudo lshw -c memory | grep capacity >> $outputFile
+echo "---CPU Tests---" >> $outputFile
+echo "-----5000" >> $outputFile
 runCPU 5000
-echo "-----10000" >> test_log.txt
+echo "-----10000" >> $outputFile
 runCPU 10000
-echo "-----20000" >> test_log.txt
+echo "-----20000" >> $outputFile
 runCPU 20000
 
-echo "---File IO Tests---" >> test_log.txt
-echo "-----2G 128Files 16Threads" >> test_log.txt
+echo "---------------------------------------------" >> $outputFile
+
+echo "---File IO Tests---" >> $outputFile
+echo "-----2G 128Files 16Threads" >> $outputFile
 runFileIO 2G
-echo "-----4G 128Files 16Threads" >> test_log.txt
+echo "-----4G 128Files 16Threads" >> $outputFile
 runFileIO 4G
 
